9a2ce7b521697e71acabccd12f7897f7189092bb
Ch1keen <gihoong7@gmail.com>
2018-08-27 17:42:54 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,50 @@
-install() {
-    if [ ! -x "/usr/bin/docker" ]; then
-        echo "Docker not found. Is docker installed?"
-	exit 1
-    fi
+# This file must be run on super user.
 
-    cp ./
-}
-
-pulseaudio="-v /var/run/dbus:/var/run/dbus"
-
-if [ $# == 0 ]; then
+help_page() {
     echo
     echo "Usage : $0 [option]"
     echo
     echo "    install   : Install firebox on this system."
     echo "    uninstall : remove firebox from this system."
+
+    exit 1
+}
+
+install() {
+    echo "!!!WARNING!!! This script will install firebox for all user."
+    echo "Please beware of this."
+    echo
+
+    # Check for docker installed
+    if [ ! -x "/usr/bin/docker" ]; then
+        echo "Docker not found. Is docker installed?"
+	exit 1
+    fi
+
+    if [ -f ./firebox ]; then
+        sudo cp ./firebox /bin/ \
+	&& echo "Succesfully Installed." \
+	|| echo "You're not permitted. Try 'sudo $0 install'." \
+
+    else
+	echo "firebox does not exist."
+    fi
+}
+
+uninstall() {
+    if [ -f /bin/firebox ]; then
+        sudo rm /bin/firebox
+
+    else
+	echo "File is missing"
+
+    fi
+}
+
+
+
+if [ $# == 0 ]; then
+    help_page
 
 elif [ $1 == "install" ]; then
     install
@@ -22,4 +52,7 @@ elif [ $1 == "install" ]; then
 elif [ $1 == "uninstall" ]; then
     uninstall
 
+else
+    echo "Cannot recognize option '$1'."
+    help_page
 fi
